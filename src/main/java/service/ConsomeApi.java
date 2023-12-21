832bac7dbbcb342a2df3022d4e3d24d501a98008
camilazci <camilazucchi@outlook.com>
2023-12-21 15:13:27 -0300

    feat: implement API request and response

--- src/main/java/service/ConsomeApi.java
+++ src/main/java/service/ConsomeApi.java
@@ -1,5 +1,23 @@
 package service;
 
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsomeApi {
 
+    public String pegaDados(String endereco) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
