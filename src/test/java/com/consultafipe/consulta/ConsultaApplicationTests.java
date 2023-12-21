ef56005a2ac35187e724cfaba77ca6d23f89ec12
camilazci <camilazucchi@outlook.com>
2023-12-21 15:01:45 -0300

    feat: add initial project

---
+++ src/test/java/com/consultafipe/consulta/ConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultafipe.consulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
