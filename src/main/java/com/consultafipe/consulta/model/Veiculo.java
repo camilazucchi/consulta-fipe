67cef544fea2493eb3251a822d7143f4c510214f
camilazci <camilazucchi@outlook.com>
2023-12-27 10:01:13 -0300

    feat: implement Veiculo record with override toString method

---
+++ src/main/java/com/consultafipe/consulta/model/Veiculo.java
@@ -0,0 +1,18 @@
+package com.consultafipe.consulta.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Veiculo(@JsonAlias("Valor") String valor,
+                      @JsonAlias("Marca") String marca,
+                      @JsonAlias("Modelo") String modelo,
+                      @JsonAlias("AnoModelo") Integer ano,
+                      @JsonAlias("Combustivel") String tipoCombustivel) {
+
+    @Override
+    public String toString() {
+        return "- Valor: " + valor + " - Marca: " + marca + " - Modelo: " + modelo + " - Ano: " + ano
+                + " - Tipo de combustível: " + tipoCombustivel;
+    }
+}
