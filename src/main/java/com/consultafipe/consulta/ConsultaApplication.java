2b48925cbd39ea4078d2b53845d75a0500e2a8d7
camilazci <camilazucchi@outlook.com>
2023-12-27 10:20:17 -0300

    refactor: remove unused exception

--- src/main/java/com/consultafipe/consulta/ConsultaApplication.java
+++ src/main/java/com/consultafipe/consulta/ConsultaApplication.java
@@ -13,7 +13,7 @@ public class ConsultaApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		Menu menu = new Menu();
 		menu.mostraMenu();
 	}
